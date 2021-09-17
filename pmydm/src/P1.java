d0ea7748befe5bb73478100fce39f7c0d915eb25
Àlvaro Higueras <higueras2292@gmail.com>
2021-09-17 16:48:46 +0200

    17/09/21

--- pmydm/src/P1.java
+++ pmydm/src/P1.java
@@ -1,2 +1,16 @@
 public class P1 {
-}
+    public static void main(String[] args)
+    {
+        String sNombre = "alb erto fer nan do";
+    }
+
+
+    String arraySeccion [] = sNombre.split(" ");
+    for(int i = 0;i < arraySeccion.lenght ; i++)
+        {
+            if(arraySeccion[i].lenght()%2 != 0)
+        {
+            System.out.println(arraySeccion[i])
+        }
+        }
+
